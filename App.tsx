2246591e8bca47b22897277c2b0c7acd4072129c
Eric Chan <eric88ordinary@gmail.com>
2021-08-26 22:53:36 +0800

    add example swiper

--- App.tsx
+++ App.tsx
@@ -2,10 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Swiper from 'components/Swiper';
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Swipe left to open camera</Text>
+      <Swiper />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +17,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
   },
